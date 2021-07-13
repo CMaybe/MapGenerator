cc3b5f851eac76ea30b3c7d3b246a82d7720aae5
김재겸 <asbsds123@gmail.com>
2021-07-13 11:26:24 +0000

    Change to merge algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <string>
 #include <fstream>
+#include <algorithm>
 
 #define H true
 #define W false
@@ -16,7 +17,15 @@
 #define ROOM	1
 #define NONE 	0
 
+typedef struct EDGE{
+	int x1;
+	int y1;
+	int x2;
+	int y2;
+}Edge;
+
 int target =5;
+int test = 0;
 int arr[HEIGHT][WIDTH];
 
 void init(){
@@ -67,8 +76,9 @@ void print(){
     writeFile.close();   
 }
 
-void BSP(int x1,int y1,int x2, int y2, bool flag, int cnt){
+Edge BSP(int x1,int y1,int x2, int y2, bool flag, int cnt){
 	if(cnt==target){
+		test= target;
 		x1 += std::rand() % 3 +1;
 		if(x1<1) x1=1;
 		if(x1>WIDTH-1) x1=WIDTH-1;
@@ -86,72 +96,64 @@ void BSP(int x1,int y1,int x2, int y2, bool flag, int cnt){
 				arr[i][j]=ROOM;
 			}
 		}
-		x1-=1; y1-=1; x2+=1; y2+=1;
-		int n= rand()%3;
-		if(n==0 || n==2){
-			int p = std::rand() % (y2-y1) + y1;
-			for(int i = x1;;i--){
-				if(arr[p][i]== WALL) break;
-				if(arr[p][i]== ROOM){
-					for(int j=x1;j>=i-1;j--){
-						arr[p][j]=ROOM;
-					}
-					break;
-				}
-			}
-			for(int i = x2;;i++){
-				if(arr[p][i]== WALL) break;
-				if(arr[p][i]== ROOM){
-					for(int j=x1;j<=i+1;j++){
-						arr[p][j]=TERNEL;
-					}
-					break;
-				}
-			}
-		}
-		if(n==1 || n==2){
-			int p = std::rand() % (x2-x1) + x1;
-			for(int i = y1;;i--){
-				if(arr[i][p]== WALL) break;
-				if(arr[i][p]== ROOM){
-					for(int j=y1;j>=i-1;j--){
-						arr[j][p]=TERNEL;
-					}
-					break;
-				}
-			}
-			for(int i = y2;;i++){
-				if(arr[i][p]== WALL) break;
-				if(arr[i][p]== ROOM){
-					for(int j=y1;j<=i+1;j++){
-						arr[j][p]=TERNEL;
-					}
-					break;
-				}
-			}
-		}
-		
-		return;
-
+		return Edge{x1,y1,x2-1,y2-1};
 	}
+	
+	int t_x1 = 0;
+	int t_x2 = 0;
+	int t_y1 = 0;
+	int t_y2 = 0;
 	if(flag == H){
 		int t = (x2+x1)/2 + (std::rand() % 4 - 2);
 		for(int i=y1;i<=y2;i++){
 			arr[i][t]=LINE;
 		}
-		BSP(x1,y1,t,y2,W,cnt+1);
-		BSP(t,y1,x2,y2,W,cnt+1);
+		Edge p1 = BSP(x1,y1,t,y2,W,cnt+1); // left
+		Edge p2 = BSP(t,y1,x2,y2,W,cnt+1); // right
+		
+		t_x1 = p1.x2;
+		t_x2 = p2.x1;
+		t_y1 = std::max(p1.y1,p2.y1);
+		t_y2 = std::min(p1.y2,p2.y2);
+		int p = 0;
+		do{
+			p = std::rand() % (t_y2-t_y1) + t_y1;
+			// break;
+		}while(arr[p][t_x1]!=ROOM || arr[p][t_x2]!=ROOM);
+		for(int i = t_x1;i<=t_x2;i++){
+			if(arr[p][i]==TERNEL) break;
+			arr[p][i]=TERNEL;
+		}
+		return Edge{p1.x1,std::max(p1.y1,p2.y1),p2.x2,std::min(p1.y2,p2.y2)};
+
+		
 	}
 	else if(flag == W){
 		int t = (y2+y1)/2 + (std::rand() % 4 - 2);
 		for(int i=x1;i<=x2;i++){
 			arr[t][i]=LINE;
 		}
-		BSP(x1,y1,x2,t,H,cnt+1);
-		BSP(x1,t,x2,y2,H,cnt+1);
+		Edge p1 = BSP(x1,y1,x2,t,H,cnt+1); // up
+		Edge p2 = BSP(x1,t,x2,y2,H,cnt+1); // down
+		
+		
+		t_x1 = std::max(p1.x1,p2.x1);
+		t_x2 = std::min(p1.x2,p2.x2);
+		t_y1 = p1.y2;
+		t_y2 = p2.y1;
+		int p = 0;
+		do{
+			p = std::rand() % (t_x2-t_x1) + t_x1;
+			break;
+		}while(arr[t_y1][p]!=ROOM || arr[t_y2][p]!=ROOM);
+		
+		for(int i = t_y1;i<=t_y2;i++){
+			if(arr[i][p]==TERNEL) break;
+			arr[i][p]=TERNEL;
+		}
+		return Edge{std::max(p1.x1,p2.x1),p1.y1,std::min(p1.x2,p2.x2),p2.y2};
 	}
-	
-	return;
+	return Edge{t_x1,t_y1,t_x2,t_y2};
 }
 	
 int main(int argc, char* argv[]) {
