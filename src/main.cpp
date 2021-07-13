38061fa926e11bb6b2fe92f211107e9a7229a737
김재겸 <asbsds123@gmail.com>
2021-07-13 13:56:25 +0000

    Implement Cellular Automata

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,13 @@
 #define WIDTH 50
 #define HEIGHT 50
 
-#define LINE	4
-#define WALL 	3
-#define TERNEL 	2
-#define ROOM	1
-#define NONE 	0
+#define PRE_WALL	6
+#define PRE_ROOM	5
+#define LINE		4
+#define WALL 		3
+#define TERNEL 		2
+#define ROOM		1
+#define NONE 		0
 
 typedef struct EDGE{
 	int x1;
@@ -26,31 +28,30 @@ typedef struct EDGE{
 
 int target =5;
 int test = 0;
-int arr[HEIGHT][WIDTH];
+int arr[HEIGHT+2][WIDTH+2];
 
-void init(){
-	for(int i = 0;i<HEIGHT;i++){
-		for(int j = 0;j<WIDTH;j++){
+void init(int tile){
+	for(int i = 0;i<HEIGHT+2;i++){
+		for(int j = 0;j<WIDTH+2;j++){
 			arr[i][j]=WALL;
 		}
 	}
 	
-	for(int i = 1;i<HEIGHT-1;i++){
-		for(int j = 1;j<WIDTH-1;j++){
-			arr[i][j]=NONE;
+	for(int i = 1;i<=HEIGHT;i++){
+		for(int j = 1;j<=WIDTH;j++){
+			arr[i][j]=tile;
 		}
 	}
 }
 
-void print(){
+void save(std::string name){
 	std::ofstream writeFile;
 	std::string output="";
-    writeFile.open("map.txt");   
-
+    writeFile.open(name.c_str());   
 
     
-	for(int i = 0;i<HEIGHT;i++){
-		for(int j = 0;j<WIDTH;j++){
+	for(int i = 0;i<HEIGHT+2;i++){
+		for(int j = 0;j<WIDTH+2;j++){
 			switch(arr[i][j]){
 				case NONE:
 					output+=" *";
@@ -62,7 +63,7 @@ void print(){
 					output+="  ";
 					break;
 				case WALL:
-					output+=" ■";
+					output+=" .";
 					break;
 				case LINE:
 					output+=" +";
@@ -81,16 +82,16 @@ Edge BSP(int x1,int y1,int x2, int y2, bool flag, int cnt){
 		test= target;
 		x1 += std::rand() % 3 +1;
 		if(x1<1) x1=1;
-		if(x1>WIDTH-1) x1=WIDTH-1;
+		if(x1>WIDTH) x1=WIDTH;
 		y1 += std::rand() % 3 + 1;
 		if(y1<1) y1=1;
-		if(y1>HEIGHT-1) y1=HEIGHT-1;
+		if(y1>HEIGHT) y1=HEIGHT;
 		x2 -= std::rand() % 3  + 1;
 		if(x2<1) x2=1;
-		if(x2>WIDTH-1) x2=WIDTH-1;
+		if(x2>WIDTH) x2=WIDTH;
 		y2 -= std::rand() % 3 + 1;
 		if(y2<1) y2=1;
-		if(y2>HEIGHT-1) y2=HEIGHT-1;
+		if(y2>HEIGHT) y2=HEIGHT;
 		for(int i=y1;i<y2;i++){
 			for(int j=x1;j<x2;j++){
 				arr[i][j]=ROOM;
@@ -144,7 +145,6 @@ Edge BSP(int x1,int y1,int x2, int y2, bool flag, int cnt){
 		int p = 0;
 		do{
 			p = std::rand() % (t_x2-t_x1) + t_x1;
-			break;
 		}while(arr[t_y1][p]!=ROOM || arr[t_y2][p]!=ROOM);
 		
 		for(int i = t_y1;i<=t_y2;i++){
@@ -155,14 +155,83 @@ Edge BSP(int x1,int y1,int x2, int y2, bool flag, int cnt){
 	}
 	return Edge{t_x1,t_y1,t_x2,t_y2};
 }
+
+void pre2Real(){
+	for(int i = 0;i<HEIGHT+2;i++){
+		for(int j = 0;j<WIDTH+2;j++){
+			if(arr[i][j]==PRE_WALL) arr[i][j] = WALL;
+			if(arr[i][j]==PRE_ROOM) arr[i][j] = ROOM;
+		}
+	}
+}
+
+void Cellular_Automata(const int wall_ratio,const int wall_effected,const int room_effected, int count){
+	int wall_cnt = (WIDTH-2)*(HEIGHT-2)*wall_ratio/100;
+	int cnt = 0;
+	while(cnt++<wall_cnt){
+		int x = std::rand() % (WIDTH-1) + 1;
+		int y = std::rand() % (WIDTH-1) + 1;
+		if(arr[y][x]!=WALL) arr[y][x]=WALL;
+		else cnt--;
+	}
+	while(count--){
+		for(int i = 1;i<=HEIGHT;i++){
+			for(int j = 1;j<=WIDTH;j++){
+				int wc = 0; // wall count;
+				if(arr[i+1][j-1]==WALL) wc++;
+				if(arr[i][j-1]==WALL) wc++;
+				if(arr[i-1][j-1]==WALL) wc++;
+				if(arr[i+1][j+1]==WALL) wc++;
+				if(arr[i][j+1]==WALL) wc++;
+				if(arr[i-1][j+1]==WALL) wc++;
+				if(arr[i+1][j]==WALL) wc++;
+				if(arr[i-1][j]==WALL) wc++;
+				if(wc >= wall_effected){
+					for(int k=i-1;k<=i+1;k++){
+						for(int w=j-1;w<=j+1;w++){
+							arr[k][w]=PRE_WALL;
+						}
+					}
+				}
+			}
+		}
+		for(int i = 2;i<HEIGHT;i++){
+			for(int j = 2;j<WIDTH;j++){
+				int rc = 0; // room count;
+				if(arr[i+1][j-1]==ROOM) rc++;
+				if(arr[i][j-1]==ROOM) rc++;
+				if(arr[i-1][j-1]==ROOM) rc++;
+				if(arr[i+1][j+1]==ROOM) rc++;
+				if(arr[i][j+1]==ROOM) rc++;
+				if(arr[i-1][j+1]==ROOM) rc++;
+				if(arr[i+1][j]==ROOM) rc++;
+				if(arr[i-1][j]==ROOM) rc++;
+				if(rc >= room_effected){
+					for(int k=i-1;k<=i+1;k++){
+						for(int w=j-1;w<=j+1;w++){
+							arr[k][w]=PRE_ROOM;
+						}
+					}
+				}
+			}
+		}
+		pre2Real();
+	}
+	
+	return;
+}
 	
 int main(int argc, char* argv[]) {
 	std::srand(std::time(nullptr));
-	init();
 	
-	BSP(1,1,WIDTH-1,HEIGHT-1,H,1);
+	init(NONE);
+	BSP(1,1,WIDTH,HEIGHT,H,1);
+	// save("BSP.txt");
+
+	init(ROOM);
+	Cellular_Automata(20,4,7,7);
+	save("Cellular_Automata.txt");
 	
-	print();
 	
 	return 0;
 }
